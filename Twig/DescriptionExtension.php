fc121268320a917f80afdc16955a38c55f6cde6d
dantleech <dan.t.leech@gmail.com>
2016-05-31 09:34:21 +0100

    Added DescriptionExtension

---
+++ Twig/DescriptionExtension.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2016 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Cmf\Bundle\ResourceBundle\Twig;
+
+use Symfony\Cmf\Component\Resource\RepositoryRegistryInterface;
+use Puli\Repository\Api\ResourceRepository;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Puli\Repository\Api\Resource\PuliResource;
+use Symfony\Cmf\Component\Resource\Description\DescriptionFactory;
+
+/**
+ * @author Daniel Leech <daniel@dantleech.com>
+ */
+class DescriptionExtension extends \Twig_Extension
+{
+    private $descriptionFactory;
+
+    /**
+     */
+    public function __construct(
+        DescriptionFactory $descriptionFactory
+    ) {
+        $this->descriptionFactory = $descriptionFactory;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('cmf_resource_description', array($this, 'getDescription')),
+        );
+    }
+
+    public function getDescription(PuliResource $resource)
+    {
+        return $this->descriptionFactory->getPayloadDescriptionFor($resource);
+    }
+
+    public function getName()
+    {
+        return 'cmf_resource_description';
+    }
+}
